cc3f13a0847db8c2b11c4cf321807db87dfd9dde
Nicholas <contactnicholas81@gmail.com>
2022-02-12 15:42:10 +0000

    Fix build errors

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,9 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+import { DocsThemeConfig } from "nextra-theme-docs/.";
+
+const config: DocsThemeConfig = {
   project: { link: "https://github.com/splashsaver/docs" }, // GitHub link in the navbar
   docsRepositoryBase: "https://github.com/splashsaver/docs/blob/master", // base URL for the docs repository
-  titleSuffix: " – Splashsaver",
-  nextLinks: true,
-  prevLinks: true,
-  search: null, // customizable, you can use algolia for example
+
   darkMode: true,
   footer: { text: `MIT ${new Date().getFullYear()} © Splashsaver` },
   editLink: { text: "Edit this page on GitHub" },
@@ -27,3 +25,5 @@ export default {
     </>
   ),
 };
+
+export default config;
